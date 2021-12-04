44a28362da5c72309ff284b919a63ffe692038ef
Trevor Day <6400186-TrevorKMDay@users.noreply.gitlab.com>
2021-12-03 22:22:01 -0600

    upload data

--- code_processing/binary-networks/combine-results_li.sh
+++ code_processing/binary-networks/combine-results_li.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-cd /home/feczk001/day00096/laterality/WTA-networks/results_li || exit
+cd /home/feczk001/day00096/laterality-wta/code_processing/binary-networks || \
+    exit
 
-for i in sub-*.csv ; do
+echo "sub,$(head -n1 "$(find results_li/ -name "sub-*.csv" | head -n1)")" > \
+    results_li.csv
+
+for i in results_li/sub-*.csv ; do
 
     sub=${i%.csv}
 
     for line in $(tail -n16 "${i}") ; do
-        echo "${sub},${line}"
+        echo "${sub},${line}" >> results_li.csv
     done
 
 done
